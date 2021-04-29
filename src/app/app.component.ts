20a7b4a91c65315e069197231d337ee25044df86
LouisBastien <bastien.louis@epitech.eu>
2021-04-29 11:18:07 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,24 @@ const ELEMENT_DATA: PeriodicElement[] = [
     symbol: "H"
   },
   { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
+  { position: 2, name: "Beryllium", weight: 4, symbol: "He" },
+  { position: 2, name: "Boron", weight: 4, symbol: "He" },
   { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
+  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
+  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
+  { position: 4, name: "Beryllium", weight: 9, symbol: "Be" },
+  { position: 4, name: "Lithium", weight: 94, symbol: "Be" },
+  { position: 4, name: "Lithium", weight: 91, symbol: "Be" },
+  { position: 4, name: "Boron", weight: 9.0122, symbol: "Be" },
   { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
+  { position: 5, name: "Carbon", weight: 11, symbol: "B" },
   { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
   { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
+  { position: 8, name: "Oxygen", weight: 16, symbol: "O" },
+  { position: 8, name: "Oxygen", weight: 16, symbol: "O" },
   { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
+  { position: 10, name: "Neon", weight: 20, symbol: "Ne" },
+  { position: 10, name: "Neon", weight: 21, symbol: "Ne" }
 ];
 
 @Component({
@@ -46,6 +56,9 @@ export class AppComponent implements OnInit, AfterViewInit {
     "weight-filter",
     "symbol-filter"
   ];
+
+  spanningColumns = ["position", "name", "weight"];
+  spans = [];
 
   // FILTER SYSTEM
   filterValues = {
@@ -68,6 +81,11 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     // FILTER SYSTEM
     this.dataSource.filterPredicate = this.createFilter();
+
+    // GROUP BY
+    this.cacheSpan("position", d => d.position);
+    this.cacheSpan("name", d => d.position + d.name);
+    this.cacheSpan("weight", d => d.position + d.name + d.weight);
   }
 
   drop(event: CdkDragDrop<string[]>) {
@@ -130,5 +148,42 @@ export class AppComponent implements OnInit, AfterViewInit {
       );
     };
     return filterFunction;
+  }
+
+  // GROUP
+
+  /**
+   * Evaluated and store an evaluation of the rowspan for each row.
+   * The key determines the column it affects, and the accessor determines the
+   * value that should be checked for spanning.
+   */
+  cacheSpan(key, accessor) {
+    for (let i = 0; i < ELEMENT_DATA.length; ) {
+      let currentValue = accessor(ELEMENT_DATA[i]);
+      let count = 1;
+
+      // Iterate through the remaining rows to see how many match
+      // the current value as retrieved through the accessor.
+      for (let j = i + 1; j < ELEMENT_DATA.length; j++) {
+        if (currentValue != accessor(ELEMENT_DATA[j])) {
+          break;
+        }
+
+        count++;
+      }
+
+      if (!this.spans[i]) {
+        this.spans[i] = {};
+      }
+
+      // Store the number of similar values that were found (the span)
+      // and skip i to the next unique row.
+      this.spans[i][key] = count;
+      i += count;
+    }
+  }
+
+  getRowSpan(col, index) {
+    return this.spans[index] && this.spans[index][col];
   }
 }
